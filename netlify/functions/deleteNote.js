2963fd362fe494ddee77c31a5564ea6dc0b97594
Omer Ahmed <omera@vt.edu>
2024-07-21 02:59:41 -0400

    configured firebase.

--- netlify/functions/deleteNote.js
+++ netlify/functions/deleteNote.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./path-to-your-firebase-adminsdk.json');
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
