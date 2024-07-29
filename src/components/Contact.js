f3d997d27667e5b2f3f5a557dbe4c93a71bc1322
Omer Ahmed <omera@vt.edu>
2024-07-29 00:10:30 -0400

    Bug Fix: sub points not being displayed correctly on note card

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,115 +1,230 @@
-import React, { useState } from 'react';
-import { Box, TextField, Button, Container, Typography, Card, CardContent, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
-import HCaptcha from '@hcaptcha/react-hcaptcha';
-import '../styles.css';
+import React, { useState, useEffect } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './Notes.css';
 
-const Contact = () => {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [status, setStatus] = useState('');
-  const [openPopup, setOpenPopup] = useState(false);
-  const [hcaptchaToken, setHcaptchaToken] = useState('');
+const Notes = () => {
+  const [notes, setNotes] = useState([]);
+  const [noteTitle, setNoteTitle] = useState('');
+  const [editorContent, setEditorContent] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
-    formData.append('h-captcha-response', hcaptchaToken);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
 
-    fetch('https://api.web3forms.com/submit', {
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.success) {
-          setName('');
-          setMessage('');
-          setHcaptchaToken(''); 
-          setOpenPopup(true); 
-        } else {
-          setStatus('Failed to send message. Please try again.');
-        }
-      })
-      .catch((error) => setStatus('Failed to send message. Please try again.'));
+  const handleAuthentication = async () => {
+    try {
+      const response = await fetch('/.netlify/functions/validatePassword', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ password }),
+      });
+      const result = await response.json();
+      if (result.isAuthenticated) {
+        setIsAuthenticated(true);
+      } else {
+        alert('Incorrect password');
+      }
+    } catch (error) {
+      console.error('Failed to authenticate:', error);
+      alert('Failed to authenticate');
+    }
   };
 
-  const handleClosePopup = () => {
-    setOpenPopup(false);
+  const fetchNotes = async () => {
+    try {
+      const response = await fetch('/.netlify/functions/getNotes');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      if (!Array.isArray(data)) {
+        throw new Error('Data is not an array');
+      }
+      setNotes(data);
+    } catch (error) {
+      console.error('Failed to fetch notes:', error);
+      alert('Failed to fetch notes');
+    }
+  };
+
+  const handleAddNote = async () => {
+    if (isAuthenticated) {
+      const note = {
+        title: noteTitle,
+        text: editorContent,
+      };
+      try {
+        const response = await fetch('/.netlify/functions/saveNote', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(note),
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setNotes([...notes, { ...note, id: data.id }]);
+        resetNoteFields();
+      } catch (error) {
+        alert('Failed to save note');
+      }
+    } else {
+      alert('You need to authenticate first');
+    }
+  };
+
+  const handleEditNote = (index) => {
+    const note = notes[index];
+    setNoteTitle(note.title);
+    setEditorContent(note.text);
+    setSelectedNote(note);
+    setIsEditing(true);
+  };
+
+  const handleDeleteNote = async (noteId) => {
+    if (isAuthenticated) {
+      try {
+        const response = await fetch(`/.netlify/functions/deleteNote?id=${noteId}`, { method: 'DELETE' });
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const newNotes = notes.filter(note => note.id !== noteId);
+        setNotes(newNotes);
+        setSelectedNote(null);
+        resetNoteFields();
+      } catch (error) {
+        alert('Failed to delete note');
+      }
+    } else {
+      alert('You need to authenticate first');
+    }
+  };
+
+  const handleUpdateNote = async () => {
+    if (isAuthenticated && selectedNote) {
+      const updatedNote = {
+        title: noteTitle,
+        text: editorContent,
+      };
+      try {
+        const response = await fetch(`/.netlify/functions/updateNote?id=${selectedNote.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedNote),
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const updatedNotes = notes.map(note => note.id === selectedNote.id ? { ...note, ...updatedNote } : note);
+        setNotes(updatedNotes);
+        resetNoteFields();
+      } catch (error) {
+        alert('Failed to update note');
+      }
+    } else {
+      alert('You need to authenticate first');
+    }
+  };
+
+  const resetNoteFields = () => {
+    setNoteTitle('');
+    setEditorContent('');
+    setSelectedNote(null);
+    setIsEditing(false);
+  };
+
+  const handleCloseNote = () => {
+    setSelectedNote(null);
+    setIsEditing(false);
   };
 
   return (
-    <Container className="contact-container">
-      <Box mb={4} display="flex" justifyContent="center">
-        <Card className="intro-card">
-          <CardContent>
-            <Typography variant="h6" component="div">
-              I would love to hear from you! Please use the form below to contact me or provide any feedback you have regarding this website. Your thoughts and inquiries are greatly appreciated.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box>
-      <form 
-        name="contact" 
-        method="POST" 
-        onSubmit={handleSubmit} 
-        className="contact-form"
-      >
-        <input type="hidden" name="access_key" value="0ef72a93-e8ea-473f-bafb-7c396b379992" />
-        <input type="hidden" name="subject" value="New Contact Form Submission" />
+    <div className="notes-container">
+      <h1>Notes</h1>
 
-        <TextField
-          label="Name"
-          variant="outlined"
-          fullWidth
-          type="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          label="Message"
-          variant="outlined"
-          fullWidth
-          name="message"
-          multiline
-          rows={4}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
+      <div className="notes-list">
+        {notes.length === 0 ? (
+          <p style={{ textAlign: 'center', fontSize: '16px', color: '#666' }}>No notes available</p>
+        ) : (
+          notes.map((note, index) => (
+            <div
+              key={index}
+              className="note-card"
+              onClick={() => setSelectedNote(note)}
+            >
+              <h3 className="note-title">{note.title}</h3>
+            </div>
+          ))
+        )}
+      </div>
 
-        <Box display="flex" flexDirection="column" alignItems="center" mt={2}>
-          <HCaptcha
-            sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
-            onVerify={(token) => setHcaptchaToken(token)}
+      {selectedNote && (
+        <div className="note-details">
+          <h2>{selectedNote.title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: selectedNote.text }} />
+          <div className="note-actions">
+            {isAuthenticated && (
+              <>
+                <button className="edit-button" onClick={() => handleEditNote(notes.findIndex(n => n.id === selectedNote.id))}>Edit</button>
+                <button className="delete-button" onClick={() => handleDeleteNote(selectedNote.id)}>Delete</button>
+              </>
+            )}
+            <button className="close-button" onClick={handleCloseNote}>Close</button>
+          </div>
+        </div>
+      )}
+
+      {isAuthenticated && (
+        <div className="note-form">
+          <input
+            type="text"
+            placeholder="Title"
+            value={noteTitle}
+            onChange={(e) => setNoteTitle(e.target.value)}
+            className="note-title-input"
           />
-          <Button type="submit" variant="contained" color="primary" className="submit-button" sx={{ mt: 2 }}>
-            Send
-          </Button>
-        </Box>
+          <ReactQuill
+            value={editorContent}
+            onChange={setEditorContent}
+            theme="snow"
+            className="rich-editor"
+          />
+          <button className="submit-button" onClick={isEditing ? handleUpdateNote : handleAddNote}>
+            {isEditing ? 'Update Note' : 'Add Note'}
+          </button>
+          {isEditing && (
+            <button className="cancel-button" onClick={resetNoteFields}>Cancel</button>
+          )}
+        </div>
+      )}
 
-        {status && <Typography variant="body1" color="textSecondary">{status}</Typography>}
-      </form>
-
-      <Dialog
-        open={openPopup}
-        onClose={handleClosePopup}
-        aria-labelledby="thank-you-dialog"
-      >
-        <DialogTitle id="thank-you-dialog">Thank You!</DialogTitle>
-        <DialogContent>
-          <Typography>
-            Your message has been sent successfully. Thank you for reaching out!
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClosePopup} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Container>
+      {!isAuthenticated && (
+        <div className="auth-section">
+          <input
+            type="password"
+            placeholder="Enter password to authenticate"
+            value={password}
+            onChange={handlePasswordChange}
+            className="auth-input"
+          />
+          <button className="auth-button" onClick={handleAuthentication}>Authenticate</button>
+        </div>
+      )}
+    </div>
   );
 };
 
-export default Contact;
+export default Notes;
