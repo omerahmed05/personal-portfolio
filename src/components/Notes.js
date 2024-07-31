51b62e95477d5a85386b8bcdcc28cb981b37d331
Omer Ahmed <omera@vt.edu>
2024-07-31 12:36:08 -0400

    updated import statement

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './Notes.css';
-import stripHtml from 'string-strip-html';
+import { stripHtml } from 'string-strip-html'; 
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
